4e30009c0053054deacc29271a4aeb08f9f1e7a3
Andy Stevens <apstevens@shayledevops.co.uk>
2020-10-09 14:42:40 +0100

    Added styles and html

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const alert = document.querySelector('');
+const send_btn = document.querySelector('');
+const message_field = document.querySelector('');
+const user = document.querySelector('');
+let userArray = ["Victoria Chambers", "", "", ""];
+let popup = document.getElementById('');
+const fullChart = document.getElementById('lineChart');
+const dailyTraffic = document.getElementById('barChart');
+const mobileChart = document.getElementById('doughnutChart');
+const trafficBar = document.querySelector('');
+// let trafficOptions = trafficNav
+
+// Data variables
+const hourLabel = ["1hr", "2hr", "3hr", "4hr", "5hr", "6hr", "7hr", "8hr"];
+const hourData = ["1", "2", "1.75", "3.5", "2.8", "4.3", "6.4", "6.8"];
+
+

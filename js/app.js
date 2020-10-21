7a5d6603d743b8c0894e61bdd40f738a0519fe90
Andy Stevens <apstevens@shayledevops.co.uk>
2020-10-21 22:10:25 +0100

    Updated css and js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,18 @@
 const alertBanner = document.getElementById("alert");
 
-alertBanner.innerHTML =
-    `
+alertBanner.innerHTML = `
     <div class="alert-banner>
         <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
     to complete</p>
         <p class="alert-banner-close">x</p>
-    </div>
-    `;
+    </div> `;
 
-    alertBanner.addEventListener('click', e => {
-        const element = e.target;
-        if (element.classList.contains("alert-banner-close")) {
-            alert.style.display = "none";
-        }
-    });
+    // alertBanner.addEventListener('click', e => {
+    //     const element = e.target;
+    //     if (element.classList.contains("alert-banner-close")) {
+    //         alert.style.display = "none";
+    //     }
+    // });
 
     const trafficCanvas = document.getElementById('traffic-chart');
 
